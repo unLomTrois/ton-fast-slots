80e82377327bd18637ac7c88383027437fc42b6f
unlomtrois <vadim.kuz02@gmail.com>
2022-07-03 12:33:19 +0300

    add basic layout

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.header__title}>Ton Fast Slots</h1>
+    </header>
+  );
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import { Layout } from "../components/Layout";
+import { Header } from "../components/Header";
+
+import styles from "./index.module.css";
 
 export default function Home() {
   return (
@@ -9,19 +12,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="header">
-        <p className="header__title">Ton Fast Slots</p>
-      </header>
+      <Header />
 
-      <main>
-        <p>
+      <main className={styles.main}>
+        {/* <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
           laudantium omnis obcaecati quaerat. Veniam, vitae. Doloremque
           perspiciatis cupiditate unde amet.
-        </p>
+        </p> */}
       </main>
 
-      <footer></footer>
+      <footer className={styles.footer}>
+        <h2 className={styles.footer__balance}>balance: 0.1488 TON</h2>
+      </footer>
     </Layout>
   );
 }
